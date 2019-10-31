7d3eb624b8f897dac3ccad82a1ec1d10eb3ea107
victorhdcoelho <victorhdcoelho@gmail.com>
2019-10-31 17:11:37 -0300

    Add dht voltage

---
+++ src/components/dashboard/ThreephasicDHTVoltage.vue
@@ -0,0 +1,22 @@
+<template>
+  <linechart
+    title="Tension"
+    url="threephase_voltage"
+    y_min="200"
+    y_max="230"
+    graphic_type="3"/>
+</template>
+
+<script>
+import LineChart from 'components/charts/LineChart.vue'
+
+export default {
+  components: {
+    linechart: LineChart
+  }
+}
+</script>
+
+<style>
+
+</style>
