4eed4191f37d93276617a7b78ff1cdac438dfec0
Pedro Cella <pedrovscella@gmail.com>
2022-07-28 17:30:32 -0300

    Criando componente TransducerEventList
    Co-authored-by: junin <nilvanperesjr@gmail.com>

---
+++ src/components/TransducerEventList.vue
@@ -0,0 +1,27 @@
+<template>
+  <button v-on:click="buttonTeste">
+    Teste
+  </button>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    buttonTeste: function () {
+      console.log("Botão apertar")
+    }
+  }   
+}
+</script>
+
+<style>
+
+</style>
