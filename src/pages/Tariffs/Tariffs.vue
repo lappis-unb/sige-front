157d7e9272fbbe9e2ef09ff3afd64b4399b46821
vitorAlves7 <vitor.alves07750@outlook.com>
2022-09-15 15:06:10 -0300

    refatorado estrutra de arquivos do Tariff e Tariffs

---
+++ src/pages/Tariffs/Tariffs.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h3 class="title">Lista de Campi</h3>
+    <div class="container">
+      <div class="q-pa-md">
+        <q-table
+          title="Campi"
+          :data="campi"
+          :columns="columns"
+          row-key="name"
+        >
+          <template v-slot:body="props">
+              <q-tr :props="props" @click="goTo(props.row.id)">
+                <q-td key="id" :props="props">{{ props.row.id }}</q-td>
+                <q-td key="name" :props="props">{{ props.row.name }}</q-td>
+              </q-tr>
+          </template>
+        </q-table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MASTER from '../services/masterApi/http-common'
+
+export default {
+  name: 'Campi',
+  data () {
+    return {
+      campi: [],
+      campus: {},
+      columns: [
+        { name: 'id', label: 'ID', align: 'left', field: row => row.id, sortable: true, style: 'width: 55px' },
+        { name: 'name', label: 'Nome', align: 'left', field: row => row.name, sortable: true }
+      ]
+    }
+  },
+  created () {
+    this.getCampi()
+  },
+  methods: {
+    getCampi () {
+      MASTER
+        .get('campi/', {})
+        .then(res => {
+          console.log(res.data)
+          this.campi = res.data
+        })
+        .catch(err => {
+          this.err = err
+          console.log('err')
+        })
+    },
+    goTo (id) {
+      this.$router.push(`/tariffs/${id}`)
+    }
+  }
+}
+</script>
+<style lang="scss" src="./styles.scss" scoped>
+
+</style>
