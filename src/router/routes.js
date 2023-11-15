5b5d01a9bb9a960a3e5a6dd0825fb49ad2b6ad7a
Carlos Rafael <carlosk2gunn@hotmail.com>
2023-11-15 19:39:12 -0300

    ✨ chore add new router view (lines)

--- src/router/routes.js
+++ src/router/routes.js
@@ -26,6 +26,7 @@ export default function (ssrContext) {
         { path: '/group_type', name: 'group_type', component: () => import('pages/Groups/GroupType.vue') },
         { path: '/groups', name: 'groups', component: () => import('pages/Groups/Groups.vue') },
         { path: '/slaves', name: 'slaves', component: () => import('pages/Slave/Slaves.vue') },
+        { path: '/lines', name: 'lines', component: () => import('pages/Line/Line.vue') },
         { path: '/transductors', name: 'transductors', component: () => import('pages/Transductor/TransductorsCRUD.vue') },
         { path: '/tariffs', name: 'tariffs', component: () => import('pages/Tariffs.vue') },
         { path: '/tariffs/:id', name: 'tariff', component: () => import('pages/Tariff.vue') }
