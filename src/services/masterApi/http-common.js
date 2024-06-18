8a6805d4c543d746d1e59be72e4ab219fa81ee98
LuizGPinheiro <lg.pinheirodev@gmail.com>
2024-06-18 02:10:33 +0000

    Update http-common.js, insert base url new API.

--- src/services/masterApi/http-common.js
+++ src/services/masterApi/http-common.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const MASTER = axios.create({
   baseURL: 'http://164.41.98.17:443/'
+  // baseURL: 'http://api.sige-homolog.lappis.rocks/api/'
   // baseURL: 'http://localhost:443/'
 })
 
