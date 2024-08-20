7ae97d43c28840cfb7e8b776ca90adc1028d0cc0
Guilherme Nishimura  Da Silva <gui.nishimura@hotmail.com>
2024-08-20 02:19:15 +0000

    Adicionando feedback

--- src/pages/Transductor.vue
+++ src/pages/Transductor.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-9">
       <div class="back-arrow" @click="goBack">
-        <!-- Ícone de seta para voltar -->
+
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00417e" width="36px" height="36px">
           <path d="M0 0h24v24H0z" fill="none"/>
           <path d="M19 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H19v-2z"/>
